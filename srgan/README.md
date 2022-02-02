cd4f8aacd0e0baeb38d37325e3f8f9a720145ac9
stselikov <69906686+stselikov@users.noreply.github.com>
2022-02-02 10:23:57 +0300

    readme

---
+++ srgan/README.md
@@ -0,0 +1,5 @@
+# Super Resulution GAN and Enchanced Super Resolution GAN test implementation
+
+В данном проекте проведена тестовая реализация следующих алгоритмов генеративных состязательных сетей:
+    - Super Resulution GAN (SRGAN)
+    - Enchanced Super Resolution GAN (ESRGAN)
--- srgan/src/esrgan/config.py
+++ srgan/src/esrgan/config.py
@@ -17,29 +17,27 @@ import random
 import gc
 
 
-LOAD_DISC = True
-LOAD_GEN = True
-
-SAVE_MODEL = True
-
-DISC_TRAIN = True # Define if we train full GAN or just pre-train Generator
+LOAD_DISC = True    # Define if we load previously saved Discriminator checkpoint
+LOAD_GEN = True     # Define if we load previously saved Generator checkpoint
+SAVE_MODEL = True   # Define if we save both Discriminator and Generator checkpoints after training
+DISC_TRAIN = True   # Define if we train full GAN or just pre-train Generator
 
 WORK_PATH = "."
-IMAGE_PATH = "DIV2K_train_HR"
-CHECKPOINT_GEN = "gen_esrgan.pth"
-CHECKPOINT_DISC = "disc_esrgan.pth"
-TEST_IMAGES = "test_images"
-RESULT_IMAGES = "result_images"
+IMAGE_PATH = "DIV2K_train_HR"           # Path to training dataset
+CHECKPOINT_GEN = "gen_esrgan.pth"       # Generator checkpoint name
+CHECKPOINT_DISC = "disc_esrgan.pth"     # Discriminator checkpoint name
+TEST_IMAGES = "test_images"             # Path to validation images
+RESULT_IMAGES = "result_images"         # Path to generated images 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LEARNING_RATE = 1e-4
-NUM_EPOCHS = 4
-BATCH_SIZE = 16
-NUM_WORKERS = 8
-HIGH_RES = 128
-LOW_RES = HIGH_RES // 4
-DISK_CONV = HIGH_RES // (2**4)
-IMG_CHANNELS = 3
-PLOT_FREQUENCY = 2
+NUM_EPOCHS = 100                        # Number of training epochs
+BATCH_SIZE = 16                         
+NUM_WORKERS = 8                         # Number of workers for Dataloader
+HIGH_RES = 128                          # Size of random cropped sub-images of distinct training images
+LOW_RES = HIGH_RES // 4                 # Size of downsampled sub-images used for training
+DISK_CONV = HIGH_RES // (2**4)          # Size of last convolution used in Discriminator model
+IMG_CHANNELS = 3                        # Number of image channels
+PLOT_FREQUENCY = 10                     # Frequency of validation images generation 
 
 # Loss function weight initialization
 pixel_weight = 1e-2 
